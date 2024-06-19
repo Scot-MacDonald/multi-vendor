c590f905db9af80ca931e707404b7a9a52023087
Scot MacDonald <scotmacdonald@hotmail.com>
2024-06-19 10:43:11 +0200

    reusable forms

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 import FormHeader from "@/app/components/backoffice/FormHeader";
+import SubmitButton from "@/app/components/formInputs/SubmitButton";
+import TextAreaInput from "@/app/components/formInputs/TextAreaInput";
 import TextInput from "@/app/components/formInputs/TextInput";
+import { generateSlug } from "@/lib/generateSlug";
 
 import React from "react";
 import { useForm } from "react-hook-form";
@@ -12,6 +15,8 @@ export default function NewCategory() {
     formState: { errors },
   } = useForm();
   async function onSubmit(data) {
+    const slug = generateSlug(data.title);
+    data.slug = slug;
     console.log(data);
   }
   return (
@@ -19,7 +24,7 @@ export default function NewCategory() {
       <FormHeader title="New category" />
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"
+        className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-transparent dark:border-lime-700 mx-auto my-3"
       >
         <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
           <TextInput
@@ -28,7 +33,19 @@ export default function NewCategory() {
             register={register}
             errors={errors}
           />
+          <TextAreaInput
+            label="Category Description"
+            name="description"
+            register={register}
+            errors={errors}
+          />
         </div>
+
+        <SubmitButton
+          isLoading={false}
+          buttonTitle="Save Category"
+          loadingButtonTitle="Creating category please wait..."
+        />
       </form>
     </div>
   );
