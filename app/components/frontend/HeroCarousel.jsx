6e479ee49cb243fd0a0716af0d90cd033f6eb596
Scot MacDonald <scotmacdonald@hotmail.com>
2024-07-29 12:28:53 +0200

    Fetching and Rendering Module Data frontpage

--- app/components/frontend/HeroCarousel.jsx
+++ app/components/frontend/HeroCarousel.jsx
@@ -3,16 +3,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Carousel } from "nuka-carousel";
-export default function HeroCarousel() {
+export default function HeroCarousel({ banners }) {
   return (
     <Carousel showArrows autoplay>
       <img src="/weed.jpg" alt="alt" />
-
-      <img src="/weed1.jpg" alt="alt" />
-
-      <img src="/weed.jpg" alt="alt" />
-
-      <img src="/weed1.jpg" alt="alt" />
+      {/* {banners.map((banner, i) => { */}
+      return (
+      {/* <Image
+            width={1500}
+            height={843}
+            src={banner.imageUrl}
+            alt={banner.title}
+            className="w-full"
+          /> */}
+      );
+      {/* })} */}
     </Carousel>
   );
 }
--- app/components/frontend/MarketList.jsx
+++ app/components/frontend/MarketList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MarketsCarousel from "./MarketsCarousel";
+import { getData } from "@/lib/getData";
 
-export default function MarketList() {
+export default async function MarketList() {
+  const markets = await getData("markets");
   return (
     <div className="py-8">
       <h2>Market List</h2>
       <div className="">
-        <MarketsCarousel />
+        <MarketsCarousel markets={markets} />
       </div>
     </div>
   );
