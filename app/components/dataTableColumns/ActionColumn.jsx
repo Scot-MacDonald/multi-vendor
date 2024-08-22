3bd4d0d4606752c0ac969cf47b1a4377fdbaa29a
Scot MacDonald <scotmacdonald@hotmail.com>
2024-08-22 12:57:26 +0200

    complete reusable table columns dataTable + markets farmers and community

---
+++ app/components/dataTableColumns/ActionColumn.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+
+export default function ActionColumn({ row, title }) {
+  const isActive = row.isActive;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Delete {title}</DropdownMenuItem>
+        <DropdownMenuItem>Edit {title}</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
